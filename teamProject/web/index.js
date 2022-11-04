3f6df1a819d5f2097115a190571b333c4786ca1f
wn01011 <wn01011@naver.com>
2022-11-04 11:06:21 +0900

    11.04 허재원
    사이드바 // 아래쪽 붙이기전

--- teamProject/web/index.js
+++ teamProject/web/index.js
@@ -2,8 +2,14 @@ const nextElem = document.getElementsByClassName(`carousel-item`)[2];
 const prevElem = document.getElementsByClassName(`carousel-item`)[0];
 const prev = document.getElementById(`prev-btn`);
 const next = document.getElementById(`next-btn`);
+const nextGenre = document.getElementsByClassName(`genre`)[2];
+const prevGenre = document.getElementsByClassName(`genre`)[0];
+const genrePrev = document.getElementById(`genre-prev-btn`);
+const genreNext = document.getElementById(`genre-next-btn`);
+
 console.log(nextElem.classList.value);
 console.log(prevElem.classList.value);
+console.log(nextGenre.classList.value);
 console.log(prevElem.classList.value == `carousel-item active`);
 
 function prevBtn() {
@@ -15,9 +21,28 @@ function prevBtn() {
 }
 
 function nextBtn() {
-  if (prevElem.classList == `carousel-item`) {
+  if (prevElem.classList.value == `carousel-item`) {
     next.classList.toggle(`off`);
   } else {
     prev.classList.toggle(`off`);
+  }
+}
+
+function genrePrevBtn() {
+  if (nextGenre.classList.value == `carousel-item genre`) {
+    genrePrev.classList.toggle(`off`);
+    console.log(`나 눌렷어`);
+  } else {
+    genreNext.classList.toggle(`off`);
+  }
+}
+
+function genreNextBtn() {
+  if (prevGenre.classList.value == `carousel-item genre`) {
+    genreNext.classList.toggle(`off`);
+    console.log(prevGenre.classList.value);
+  } else {
+    genrePrev.classList.toggle(`off`);
+    console.log(prevGenre.classList.value);
   }
 }
