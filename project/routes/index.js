dd2c71adc128cc5212eb62cffe79bdff3989a7b5
hjw <hjwl@naver.com>
2022-11-09 20:36:58 +0900

    11.09
    chart에 db를 개인적으로 만들고 // 그거에 대한 정보를 불러 올 수 있게 만듬
    playListIn에 db에 정보를 만들고/ 그거를 옮길수 있게 함 playListIn에 db에 유저 아이디가 키로 적혀있어서 같이 써지지가 않음 db에서 수정 할 내용이 있을것 같다.

--- project/routes/index.js
+++ project/routes/index.js
@@ -7,6 +7,7 @@ const playlist = require("./playList.js");
 // const user = require("./user.js");
 // const musicUpload = require("./musicUpload.js");
 const chart = require("./chart.js");
+const playlist = require("./playList.js");
 
 // router.use("/", (req, res, next) => {
 //   console.log(req.body, req.query);
