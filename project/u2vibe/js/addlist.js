036a41ce338f4fc29abe4d17537b42932ac1e675
Nyes98 <111405218+Nyes98@users.noreply.github.com>
2022-11-09 21:40:56 +0900

    1109 project add
    addplaylist edit

--- project/u2vibe/js/addlist.js
+++ project/u2vibe/js/addlist.js
@@ -1,16 +1,17 @@
-let addplaylist = document.getElementById("add-playlist");
-let inputtitle = document.getElementById("input-title");
-let inputcontents = document.getElementById("input-contents");
-const addbtn = document.getElementById("add-btn");
-
 let userprofileid = document.getElementById("userprofile-id");
-
 let logincheck = document.cookie.split("=")[1].split(".")[1];
+let listtitle = document.getElementById("list-title");
+let listcontents = document.getElementById("list-contents");
+let listimg = document.getElementById("list-img");
+const listpost = document.getElementById("list-post");
+const mordalinputtitle = document.getElementById("mordal-input-title");
+const mordalinputcontents = document.getElementById("mordal-input-contents");
+
+const curuserName = JSON.parse(
+  window.atob(document.cookie.split("=")[1].split(".")[1])
+).id;
 
 if (logincheck) {
-  const curuserName = JSON.parse(
-    window.atob(document.cookie.split("=")[1].split(".")[1])
-  ).id;
   userprofileid.innerText = curuserName;
 }
 document.getElementById("logout-btn").onclick = async function (e) {
@@ -31,7 +32,42 @@ function removelist() {
   console.log("dot작동");
 }
 
-inputtitle.oninput = function () {
-  addbtn.classList.add("on");
-  if (inputtitle.value == "") addbtn.classList.remove("on");
-};
+async function addedlist() {
+  const data = await axios.post("/api/upload/addedlist", {
+    id: curuserName,
+  });
+  console.log(data.data.info);
+  console.log(data.data.info.musicName);
+
+  listtitle.innerHTML = data.data.info.musicName;
+  listcontents.innerHTML = data.data.info.singer;
+  listimg.innerHTML = `
+  <img src="../../../upload/${data.data.info.albumImg}" alt="imsi" id="list-img" />
+  `;
+}
+
+addedlist();
+
+function makePlaylist() {
+  listpost.innerHTML = `
+   <div class="list-click">
+    <div class="list-img">
+      <img src="../../assets/img/newplaylist.png" alt="newplaylist" />
+      <a href="#">
+        <div class="list-hidden">
+          <div class="icon-box">
+            <img src="../../assets/img/playbtn.png" alt="play" />
+            <img
+              src="../../assets/img/delbtn.png"
+              alt="dot"
+              onclick="removelist()"
+            />
+          </div>
+        </div>
+      </a>
+    </div>
+    <div class="list-title">${mordalinputtitle.value}</div>
+  </div>
+  <div class="list-contents">0곡</div>
+`;
+}
--- project/u2vibe/js/index.js
+++ project/u2vibe/js/index.js
@@ -1,6 +1,15 @@
 let userprofileid = document.getElementById("userprofile-id");
-
 let logincheck = document.cookie.split("=")[1].split(".")[1];
+let mordaladdptn = document.getElementById("mordal-add-btn");
+let inputtitle = document.getElementById("mordal-input-title");
+let inputcontents = document.getElementById("mordal-input-contents");
+const addbtn = document.getElementById("mordal-add-btn");
+const canclebtn = document.getElementById("mordal-cancle-btn");
+const mordalroot = document.getElementById("mordal-root");
+const addplaylist = document.getElementById("add-playlist");
+const listboard = document.getElementById("list-board");
+const mordalinputtitle = document.getElementById("mordal-input-title");
+const mordalinputcontents = document.getElementById("mordal-input-contents");
 
 if (logincheck) {
   // logoutbox.classList.remove("on");
@@ -22,3 +31,49 @@ document.getElementById("logout-btn").onclick = async function (e) {
   }
   location.href = "http://localhost:8080/";
 };
+
+inputtitle.oninput = function () {
+  addbtn.classList.add("on");
+  if (inputtitle.value == "") addbtn.classList.remove("on");
+};
+
+canclebtn.addEventListener("click", () => {
+  mordalroot.classList.add("on");
+});
+
+// addplaylist.addEventListener("click", () => {
+//   });
+addplaylist.onclick = () => {
+  mordalroot.classList.toggle("on");
+  console.log("addplay?");
+};
+window.addEventListener("click", (e) => {
+  console.log(e.target);
+});
+
+function makePlaylist() {
+  const tempElem = document.createElement("div");
+  tempElem.classList.add("list-post");
+  tempElem.innerHTML = `<div class="list-click">
+     <div class="list-img">
+       <img src="../../assets/img/newplaylist.png" alt="newplaylist" />
+       <a href="#">
+         <div class="list-hidden">
+           <div class="icon-box">
+             <img src="../../assets/img/playbtn.png" alt="play" />
+             <img
+               src="../../assets/img/delbtn.png"
+               alt="dot"
+               onclick="removelist()"
+             />
+           </div>
+         </div>
+       </a>
+     </div>
+     <div class="list-title">${mordalinputtitle.value}</div>
+     </div>
+     <div class="list-contents">0곡</div>`;
+  listboard.appendChild(tempElem);
+  console.log("들어오냐");
+  mordalroot.classList.add("on");
+}
