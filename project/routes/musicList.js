e0eda68a919af22e51b896c662f8c08989dcc768
hjaewon1234 <hjwl@naver.com>
2022-11-10 12:43:10 +0900

    11.10
    쓸모없는 폴더 다지움

--- project/routes/musicList.js
+++ project/routes/musicList.js
@@ -3,7 +3,7 @@ const router = require("express").Router();
 const { MusicList } = require(`../models/index.js`);
 
 router.post(`/list`, async (req, res) => {
-  console.log(MusicList[0].userId + `디비에 뭐잇냐`);
+  console.log(`!` + MusicList.id + `! 디비에 뭐잇냐`);
   console.log(req.body.userId + `플레이리스트 받은 내용`);
   const listUp = await MusicList.findAll();
   //   where: {
